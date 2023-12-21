257002f4928240ea8dac5fad6e177d110ee908b9
AdarshDev9349 <adarshdev9349@gmail.com>
2023-12-21 12:05:54 +0530

    commit

--- oops.dart
+++ oops.dart
@@ -8,7 +8,6 @@ class Car {
     
   }
 }
-
 class Person {
   String name;
   List ownedCars;
@@ -35,14 +34,14 @@ class Person {
 }
 
 main() {
-  var Newcar = Car("Omni", 300);
-  var Bro = Person("Achu", [], 1000);
-  Bro.Buycar(Newcar);
+  var Newcar = Car("Omni", 300); //car name and price
+  var Bro = Person("Achu", [], 1000);//bro's name is achu with some amount in his account
+  Bro.Buycar(Newcar); //bro trying to buy a car
   print("BUYING A CAR");
   print("${Bro.name} bought a ${Newcar.name} for ${Newcar.price}");
   print("${Bro.name} has ${Bro.Moneyleft} left in his account.");
   Newcar.changePrice(1000);
-  Bro.sellcar(Newcar);
+  Bro.sellcar(Newcar); //bro selling his car
   print("SELLING YOUR CAR");
   print("${Bro.name} sold his car for ${Newcar.price}");
   print("${Bro.name} has ${Bro.Moneyleft} in his account");
